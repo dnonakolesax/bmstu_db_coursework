7b009e029110b4deaa09451f5fa954a373d92a00
dnonakolesax <90285641+dnonakolesax@users.noreply.github.com>
2022-11-19 23:53:44 +0300

    Update auth_test.py

--- test/auth_test.py
+++ test/auth_test.py
@@ -9,7 +9,7 @@ options.add_argument('--disable-dev-shm-usage')
 options.add_argument('--headless')
 options.add_argument('--start-maximized')
 
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(options=options)
 
 driver.get('localhost:5001')
 
