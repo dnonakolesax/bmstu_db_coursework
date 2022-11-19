bdee6c6ed121416fbe04503acb2c237652b1f1fb
dnonakolesax <90285641+dnonakolesax@users.noreply.github.com>
2022-11-19 23:51:59 +0300

    Update auth_test.py

--- test/auth_test.py
+++ test/auth_test.py
@@ -1,5 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+
+options = Options()
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-gpu')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--headless')
+options.add_argument('--start-maximized')
 
 driver = webdriver.Chrome()
 
