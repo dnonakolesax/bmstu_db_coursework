fd314ccbe88887f17c92a55e779ce80bc00f6c7e
dnonakolesax <90285641+dnonakolesax@users.noreply.github.com>
2022-11-20 00:47:19 +0300

    Update auth_test.py

--- test/auth_test.py
+++ test/auth_test.py
@@ -14,7 +14,7 @@ driver = webdriver.Chrome(options=options)
 
 driver.get('localhost:5001')
 
-driver.find_element(By.XPATH, 'safgaafs')
+driver.find_element(By.XPATH, 'safgaaf')
 
 driver.close()
 
